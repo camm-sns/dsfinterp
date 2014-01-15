d648cfab6df6bfe833fa6c74752bc06ce86bc41f
Jose Borreguero <borreguero@gmail.com>
2014-01-15 16:25:51 -0500

    Tests divided into suites

--- dsfinterp/test/dsfload_test.py
+++ dsfinterp/test/dsfload_test.py
@@ -1,28 +1,35 @@
 '''
 Created on Jan 14, 2014
 
-@author: jbq
+@author: Jose Borreguero
 '''
 import unittest
+from dsfload import DsfLoaderMantidWorkspace2D
 
 try:
-  import mantid
+  import mantid.simpleapi as mapi
 except ImportError:
   print 'mantid library not found!'
   raise
 
 class DsfLoadTest(unittest.TestCase):
 
-  def testDsfLoaderMantidWorkspace2D(self):
-    from dsfload import DsfLoaderMantidWorkspace2D
-    ws = mantid.LoadNexus('data/exp100K.nxs') #workspace with 9 Q-values and 700-Evalues
+  def test_DsfLoaderMantidWorkspace2D(self):
+    ''' Load a Nexus file containing a workspace2D with data '''
+    ws = mapi.LoadNexus(Filename='./data/exp100K.nxs') #workspace with 9 Q-values and 700-Evalues
     loader = DsfLoaderMantidWorkspace2D()
     intensities,errors = loader.Load(ws)
     nQ, nE = intensities.shape
     self.assertEqual(nQ, 9)
     self.assertEqual(nE, 700)
-    self.assertEqual(intensities[4][321],0.0302994) #just one simple example
+    self.assertEqual(intensities[4][321], 0.0302994) #just one simple example
+    self.assertEqual(errors[4][321], 0.001094)
+
+def suite():
+  loader = unittest.TestLoader()
+  suite = unittest.TestSuite()
+  suite.addTest(loader.loadTestsFromTestCase(DsfLoadTest))
+  return suite
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+  unittest.TextTestRunner(verbosity=2).run(suite())
