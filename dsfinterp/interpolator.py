710d21aa991ece925b94a21e61c504dbb49e267e
Jose Borreguero <borreguero@gmail.com>
2014-01-17 15:20:21 -0500

    Added test for interpolator

--- dsfinterp/interpolator.py
+++ dsfinterp/interpolator.py
@@ -4,7 +4,8 @@ Created on Jan 6, 2014
 @author: jbq
 '''
 import numpy
-from logger import vlog
+from logger import vlog, tr
+import copy
 
 class Interpolator(object):
   '''
@@ -16,9 +17,6 @@ class Interpolator(object):
   def __init__(self, fseries, signalseries, errorseries=None, running_regr_type = 'linear' ):
     '''
     Arguments:
-      fseries: list of values for the external parameter
-      signalseries: values of the structure factor as a function of the external parameter
-      [errorseries]: associated errors of the structure factor
       [running_regr_type]: method for the local, runnig regression
     
     Attributes:
@@ -35,7 +33,7 @@ class Interpolator(object):
 
     self.running_regr_type = running_regr_type
     self.range = ( fseries[ 0 ], fseries[ -1 ] )
-    # Do running regression and estimate errors
+    # Do running regression, and if neccessary estimate errors
     if running_regr_type == 'linear':
       from scipy.stats import linregress
       windowlength = 5 # important to be odd number
@@ -44,33 +42,67 @@ class Interpolator(object):
       else:
         # Lower boundary, the first three values
         x = fseries[ : windowlength ]
-        y = self.signalseries[ : windowlength ]
+        y = signalseries[ : windowlength ]
         slope, intercept, r_value, p_value, std_err = linregress( x, y )
         linF = lambda xx: intercept + slope * xx
-        self.fitted = [ linF( x[0] ), linF( x[1] ), linF( x[2] ) ]
-        self.errors = [ std_err, std_err, std_err ]
+        self.fitted = [ linF(x[0]), linF(x[1]), linF(x[2]) ]
+        residuals = numpy.square(numpy.vectorize(linF)(x) - y)
+        residual = numpy.sqrt( numpy.mean(residuals)) #average residual
+        self.errors = [ residual, residual, residual]
         # Continue until hitting the upper boundary
         index = 1 # lower bound of the regression window
-        while ( index + windowlength < len( fseries ) ):
+        while ( index + windowlength <= len( fseries ) ):
           x = fseries[ index : index + windowlength ]
-          x = self.signalseries[ index : index + windowlength ]
+          y = signalseries[ index : index + windowlength ]
           slope, intercept, r_value, p_value, std_err = linregress( x, y )
           linF = lambda xx: intercept + slope * xx
-          self.fitted.append( linF( x[2] ))
-          self.errors.append( std_err )
+          self.fitted.append(linF(x[2]))
+          residuals = numpy.square(numpy.vectorize(linF)(x) - y)
+          residual = numpy.sqrt( numpy.mean(residuals)) #average residual
+          self.errors.append(residual)
           # Resolve the upper boundary
           if index + windowlength +1 == len( fseries ):
-            self.fitted += [ linF( x[3] ), linF( x[4] ) ]
-            self.errors += [ std_err, std_err ]
+            self.fitted += [ linF(x[3]), linF(x[4]) ]
+            self.errors += [ residual, residual ]
+          index += 1
+    elif running_regr_type == 'quadratic':
+      from numpy import polyfit
+      windowlength = 5 # important to be odd number
+      if len( fseries ) < windowlength:
+        vlog.error( 'series has to contain at least {0} members'.format( windowlength ) )
+      else:
+        # Lower boundary, the first three values
+        x = fseries[ : windowlength ]
+        y = signalseries[ : windowlength ]
+        coeffs, residuals, rank, singular_values, rcond= polyfit(x,y,2, full=True) #second order polynomial
+        quadF = lambda xx: coeffs[0]*xx*xx + coeffs[1]*xx + coeffs[2]
+        self.fitted = [ quadF(x[0]), quadF(x[1]), quadF(x[2]) ]
+        residual = numpy.sqrt(numpy.mean( residuals )) #average residual
+        self.errors = [ residual, residual, residual]
+        # Continue until hitting the upper boundary
+        index = 1 # lower bound of the regression window
+        while ( index + windowlength <= len( fseries ) ):
+          x = fseries[ index : index + windowlength ]
+          y = signalseries[ index : index + windowlength ]
+          coeffs, residuals, rank, singular_values, rcond = polyfit(x,y,2, full=True) #second order polynomial
+          quadF = lambda xx: coeffs[0]*xx*xx + coeffs[1]*xx + coeffs[2]
+          self.fitted.append(quadF(x[2]))
+          residuals = numpy.square(numpy.vectorize(quadF)(x) - y)
+          residual = numpy.sqrt( numpy.mean(residuals)) #average residual
+          self.errors.append(residual)
+          # Resolve the upper boundary
+          if index + windowlength +1 == len( fseries ):
+            self.fitted += [ quadF(x[3]), quadF(x[4]) ]
+            self.errors += [ residual, residual ]
+          index += 1
     else:
-      vlog.warning( 'Only the running linear regression has been implemented to date' )
+      vlog.warning( 'Requested regression type not recogized' )
 
-    if errorseries:
+    if errorseries is not None:
       self.errors = errorseries
-
     # Interpolators for fitted and errors
     from scipy.interpolate import interp1d, UnivariateSpline
-    x = numpy.array( self.fvalues )
+    x = numpy.array( fseries )
     y = numpy.array( self.fitted )
     e = numpy.array( self.errors )
     w = 1.0 / e
@@ -81,8 +113,7 @@ class Interpolator(object):
   def __call__(self, fvalue):
     ''' Evalue the interpolators for the particular value of the external parameter '''
     if self.range[0] <= fvalue <= self.range[1]:
-      x = numpy.array( [fvalue,] )
-      return self.y(x)[0], self.e(x)[0]
+      return self.y(fvalue), float(self.e(fvalue))
     else:
       vlog.error( 'Value outside of interpolating bounds' )
       return ( float( 'inf' ), float( 'inf' ) )
--- dsfinterp/test/interpolator_test.py
+++ dsfinterp/test/interpolator_test.py
@@ -7,23 +7,123 @@ import unittest
 import numpy
 
 from interpolator import Interpolator
+from logger import tr
 
 class TestInterpolator(unittest.TestCase):
 
-  def ServeInput(self):
-    ''' Helper method to serve data for the test cases '''
+  def LoadSeries(self, infile):
+    ''' Helper method, loads f-values, signals, and errors from a three-column file '''
+    fseries = []
+    signalseries = []
+    errorseries = []
+    for line in open(infile).readlines():
+      if line[0]=='#': continue
+      f, y, e = [float(x) for x in line.split()]
+      fseries.append(f)
+      signalseries.append(y)
+      errorseries.append(e)
+    return fseries,signalseries,errorseries
+
+  def RecordTriad(self, fseries, signalseries, errorseries, outfile):
+    ''' Helper method, saves to a file. For debugging purposes '''
+    size = len(fseries)
+    buf='# f signal error\n'
+    for i in range(size):
+      buf += '{0} {1} {2}\n'.format(fseries[i],signalseries[i],errorseries[i])
+    open(outfile,'w').write(buf)
+
+  def ServeInput(self, create_errorseries=False, outfile=None):
+    ''' Helper method to serve data for the test cases 
+    
+    Arguments:
+      [create_errorseries]: also generate a list of errors
+      [outfile]: save the series to a file, for debugging purposes
+    '''
     size = 100
+    fseries = 1.0*numpy.arange(size)
     function = numpy.sin(2*numpy.pi*numpy.arange(size)*0.01)
     noise = 0.2*numpy.random.rand(size)
-    signalseries = function + noise
-    errorseries = 0.2*numpy.random.rand(size)
-    fseries = 1.0*numpy.arange(size)
-    return signalseries, errorseries, fseries
+    signalseries = numpy.abs(function + noise)
+    errorseries = None
+    if create_errorseries:
+      errorseries = 0.2*numpy.random.rand(size)
+    if outfile:
+      buf='#f  signal error\n'
+      for i in range(size):
+        buf += '{0} {1}'.format(fseries[i],signalseries[i])
+        if create_errorseries:
+          buf += ' {0}'.format(errorseries[i])
+        buf +='\n'
+      open(outfile,'w').write(buf)
+    return fseries, signalseries, errorseries
 
   def test___init__(self):
-    signalseries, errorseries, fseries = self.ServeInput()
-    interpolator = Interpolator(signalseries, errorseries, fseries)
+    ''' Just run the linear and quadratic interpolations '''
 
+    fseries, signalseries, errorseries = self.ServeInput()
+    interpolator = Interpolator(fseries, signalseries, running_regr_type = 'linear')
+    y_interpolated = interpolator.y(fseries)
+    e_interpolated = interpolator.e(fseries)
+
+    fseries, signalseries, errorseries = self.ServeInput(create_errorseries=True)
+    interpolator = Interpolator(fseries, signalseries, running_regr_type = 'linear')
+    y_interpolated = interpolator.y(fseries)
+    e_interpolated = interpolator.e(fseries)
+
+    fseries, signalseries, errorseries = self.ServeInput()
+    interpolator = Interpolator(fseries, signalseries, running_regr_type = 'quadratic')
+    y_interpolated = interpolator.y(fseries)
+    e_interpolated = interpolator.e(fseries)
+
+    fseries, signalseries, errorseries = self.ServeInput(create_errorseries=True)
+    interpolator = Interpolator(fseries, signalseries, running_regr_type = 'quadratic')
+    y_interpolated = interpolator.y(fseries)
+    e_interpolated = interpolator.e(fseries)
+
+  def test__call__(self):
+    ''' Just make sure the object is callable '''
+    fseries, signalseries, errorseries = self.ServeInput()
+    interpolator = Interpolator(fseries, signalseries, running_regr_type = 'linear')
+    interpolator(numpy.mean(fseries))
+    # Test the bounding errors
+    self.assertEqual( interpolator(min(fseries)-1.0), (float('inf'),float('inf')) )
+    self.assertEqual( interpolator(max(fseries)+1.0), (float('inf'),float('inf')) )
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 def suite():
   loader = unittest.TestLoader()
   suite = unittest.TestSuite()
