50ecad677d1366a7abee391e0648987b49a1c507
Jose Borreguero <borreguero@gmail.com>
2014-02-12 15:35:31 -0500

    Added a manifest file

--- setup.py
+++ setup.py
@@ -4,36 +4,20 @@ Created on Jan 15, 2014
 @author: Jose Borreguero
 '''
 
-from setuptools import setup, find_packages
+
+from setuptools import setup
 
 setup(
   name = 'dsfinterp',
+  packages = ['dsfinterp','dsfinterp/test' ],
   version = '0.1',
-
-  # Package structure
-  #
-  # find_packages searches through a set of directories 
-  packages = find_packages('dsfinterp', exclude = ['ez_setup','*.tests', '*.tests.*', 'tests.*', 'tests']),
-  # package_dir directive maps package names to directories.
-  package_dir = {'': 'dsfinterp'},
-
-  # Tests
-  #
-  # Tests must be wrapped in a unittest test suite by either a
-  # function, a TestCase class or method, or a module or package
-  # containing TestCase classes. If the named suite is a package,
-  # any submodules and subpackages are recursively added to the
-  # overall test suite.
-  test_suite = 'dsfinterp.tests.suite',
-
-  # Meta information
-  author = 'Jose Borreguero',
-  author_email = 'borreguero@gmail.com',
   description = 'Cubic Spline Interpolation of Dynamics Structure Factors',
   long_description = open('README.md').read(),
+  author = 'Jose Borreguero',
+  author_email = 'jose@borreguero.com',
   url = 'https://github.com/camm-sns/dsfinterp',
   download_url = 'http://pypi.python.org/pypi/dsfinterp',
-  keywords = ['dynamic structure factor', 'interpolation', 'spline'],
+  keywords = ['AMBER', 'mdend', 'energy', 'molecular dynamics'],
   classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
@@ -42,5 +26,5 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Topic :: Scientific/Engineering :: Physics',
-  ],
+    ],
 )
